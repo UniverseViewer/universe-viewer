759f6605cc73ac0c9eff7cf42756393758890b47
Mathieu ABATI <mathieu.abati@gmail.com>
2025-12-27 10:53:46 +0100

    Add 2MASS Redshift Survey catalog

---
+++ public/catalogs/twomassrsc/README.md
@@ -0,0 +1,24 @@
+# TWOMASSRSC - 2MASS Redshift Survey (2MRS) Catalog
+
+It has been converted to UniverseViewer data format using the `extract.py` script.
+
+The result has been compressed with:
+```
+gzip -c 2mrs.dat > 2mrs.gz
+```
+
+To run the conversion script, you have to download the table 3 `J_ApJS_199_26_table3.dat.gz.fits` file from https://cdsarc.cds.unistra.fr/viz-bin/cat/J/ApJS/199/26#/browse URL.
+
+## Acknowledgments
+
+The 2MASS Redshift Survey - Description and Data Release
+
+Huchra J.P., Macri L.M., Masters K.L., Jarrett T.H., Berlind P.,\
+Calkins M., Crook A.C., Cutri R., Erdogdu P., Falco E., George T.,\
+Hutcheson C.M., Lahav O., Mader J., Mink J.D., Martimbeau N.,\
+Schneider S., Skrutskie M., Tokarz S., Westover M.\
+\<Astrophys. J. Suppl. 199, 26 (2012)\>
+
+## References
+
+- [TWOMASSRSC - 2MASS Redshift Survey (2MRS) Catalog](https://heasarc.gsfc.nasa.gov/W3Browse/all/twomassrsc.html)
---
+++ public/catalogs/twomassrsc/extract.py
@@ -0,0 +1,59 @@
+"""
+ * Copyright (C) 2025 Mathieu Abati <mathieu.abati@gmail.com>
+ * Copyright (C) 2025 Roland Triay <triay@cpt.univ-mrs.fr>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+"""
+
+from astropy.io import fits
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from sys import argv, exit, stderr
+
+fits_file = "J_ApJS_199_26_table3.dat.gz.fits"
+dat_file = "2mrs.dat"
+
+max_qso = None
+
+print(f"Loading {fits_file} ...", end="", flush=True)
+with fits.open(fits_file) as hdul:
+    data = hdul[1].data
+
+    if max_qso is not None:
+        data = data[:max_qso]
+
+    print(f" {len(data):,} objects selected")
+
+    ra_list = []
+    dec_list = []
+    z_list = []
+
+    for row in tqdm(data, desc="Processing objects"):
+        ra_list.append(np.deg2rad(row["RAdeg"]))
+        dec_list.append(np.deg2rad(row["DEdeg"]))
+        z_list.append(row["cz"])
+
+    df = pd.DataFrame({
+        "RA_rad": ra_list,
+        "DEC_rad": dec_list,
+        "z": z_list
+    })
+
+    print(f"Writing output file {dat_file} ...", end="", flush=True)
+    df.to_csv(dat_file, sep=" ", header=False, index=False)
+    print(" done.")
+
